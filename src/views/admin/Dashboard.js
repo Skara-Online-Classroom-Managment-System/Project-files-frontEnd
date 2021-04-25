d24294594b61b8291b8aae0d629023200f21737e
Ayvak12 <56349596+Ayvak12@users.noreply.github.com>
2021-04-25 07:18:09 +0400

    src

---
+++ src/views/admin/Dashboard.js
@@ -0,0 +1,37 @@
+import React from "react";
+// import axios from 'axios';
+// import{Link} from 'react-router-dom';
+// // components
+
+// import CardPageVisits from "components/Cards/CardPageVisits.js";
+// import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+// import StudentAdminNavbar from "components/Navbars/StudentAdminNavbar.js";
+// import TeacherAdminNavbar from "components/Navbars/TeacherAdminNavbar.js";
+// import Sidebar from "components/Sidebar/Sidebar.js";
+
+// import CardStats from "components/Cards/CardStats.js";
+
+export default function Dashboard() {
+  
+
+  return (
+    <>
+ 
+
+  return (
+    <>
+       {/* <div className='flex flex-wrap'></div>
+      <div className='flex flex-wrap mt-4'>
+        <div className='w-full xl:w-8/12 mb-12 xl:mb-0 px-4'>
+          <CardPageVisits />
+        </div>
+        <div className='w-full xl:w-4/12 px-4'>
+          <CardSocialTraffic />
+        </div>
+      </div> } */}
+      Hi this is the dashboard.......
+    </>
+  );     
+      </>
+  );
+}
